e066eca10d92e5e325792726b41e426549e7eceb
Kevin <kevin.ramirezcorr@campusucc.edu.co>
2025-08-12 15:19:09 +0000

    Proyecto de conversión de datos

---
+++ proyecto2/Main.java
@@ -0,0 +1,10 @@
+package proyecto2;
+
+public class Main {
+    public static void main(String[] args) {
+        double potencia = Math.pow(5,3);
+        int potenciaEntera = (int) potencia;
+        System.out.println(potencia);
+        System.out.println(potenciaEntera);
+    }
+}

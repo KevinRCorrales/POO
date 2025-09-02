7bbad892e6f0367c9d218a16f771ef2dee25de28
Kevin <kevin.ramirezcorr@campusucc.edu.co>
2025-09-02 10:07:33 -0500

    Ejercicio de ciclos 5.1

---
+++ ciclos/src/ej51.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ej51 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int cuenta = 0;
+        int sumaNumeros = 0;
+        int numero;
+        do{
+            System.out.print("Ingresa un número: ");
+            numero = input.nextInt();
+            if (numero > 0) {
+                sumaNumeros += numero;
+                cuenta++;
+            }
+        } while (numero > 0);
+        if (sumaNumeros == 0) {
+            System.err.println("No se ingresaron valores positivos");
+        } else {
+            System.out.println("Media: " +  (double) sumaNumeros / (double) cuenta);
+        }
+        input.close();
+    }
+}

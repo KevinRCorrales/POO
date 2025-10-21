57ae87d9ae1ce5d80776763aa4c22da577b740e7
Kevin <kevin.ramirezcorr@campusucc.edu.co>
2025-10-21 08:57:35 -0500

    Herencia y polimorfismo

---
+++ Herencia/src/Animal.java
@@ -0,0 +1,5 @@
+public class Animal {
+    public void hacerSonido(){
+        System.out.println("Sonido genérico.......");
+    }
+}
---
+++ Herencia/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal mateo = new Perro();
+        Animal angustin = new Gato();
+
+        mateo.hacerSonido();
+        angustin.hacerSonido();
+    }
+}
